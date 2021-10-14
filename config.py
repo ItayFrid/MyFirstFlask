09c618381e9519abeb1eaed804458f3c42e2588c
Itay Fridman <fitay123@gmail.com>
2021-10-14 10:10:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    SECRET_KEY          = '\xcd\x0c\xea\x17q\xb9\xcb\xbb\xa7:\x97\xd6x\x10\x01\xef\xe8\xd2\x90\xb3[\xb0\x92\xda'
-    CONSUMER_KEY        = 'XkzkavT1AYCbdKl4YtL5uzYyd'
-    CONSUMER_SECRET     = 'mxH3coDZh9iK5IQfOqCcBHlBXJqQPESyimOzqnzTK8uhsXDXd1'
-    ACCESS_TOKEN        = '1318107410-W35PhlRbyRkrAcoBqzKi5a8yLPe1WjrF7wqPdz7'
-    ACCESS_TOKEN_SECRET = 'hLWdwiFo2R4iL2xjdIZuQYwEjmbvjZvOEGJ5EJzDF5v2h'
+    SECRET_KEY          = ''
+    CONSUMER_KEY        = ''
+    CONSUMER_SECRET     = ''
+    ACCESS_TOKEN        = ''
+    ACCESS_TOKEN_SECRET = ''
